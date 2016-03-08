d562d90fd224be27d7e79475d21642fcf0c95c5d
Luis SEVERINO <lfs.severino@gmail.com>
2016-03-08 01:23:57 +0100

    update usage

--- mgit.sh
+++ mgit.sh
@@ -3,7 +3,13 @@
 # Add simple help
 help()
 {
-  echo 'Run: ./mgit.sh -b <branch> -a <pull|push|none> -m <message>'
+  echo 'Ex: ./mgit.sh -b <branch> -pull -push -m "<message> -path <path>"'
+  echo ''
+  echo '	-b <branch> (target repository branch) >>> default: master'
+  echo '	-pull (Fetch from and integrate with your local branch before commit) '
+  echo '	-push (Update remote refs along with associated objects after commit) '
+  echo '	-m "<your message>" (Define a commit message) '
+  echo '	-path <relative_path> (Enter absolute paths or relative paths) >>> default: .'
   exit
 }
 
@@ -11,7 +17,23 @@ mgit()
 {
   directories=$(ls -d */)
   currwordir=$(pwd)
-  
+  pull=false
+  push=false 
+  branch="master"
+  path="."
+
+paste=vpaste	# default is vertical pasting
+seplist="\t"	# default separator is tab
+
+  set -- $(getopt -b $0 d:s "$@")
+  for o
+  do	case "$o" in
+	-b)	shift; echo $1; shift;;
+	-path)	echo $1; shift;;
+	esac
+  done
+
+  exit 1;
   for dir in $directories
   do
     cd $dir
